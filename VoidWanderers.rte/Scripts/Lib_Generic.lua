cb26d69f64a09a797b08abcbf5f4aa252c498ca6
Bit-Crust <166267954+Bit-Crust@users.noreply.github.com>
2024-04-06 21:22:14 -0500

    Update Lib_Generic.lua
    Add limb replacement attempt function to globals (from Limb.lua).

--- VoidWanderers.rte/Scripts/Lib_Generic.lua
+++ VoidWanderers.rte/Scripts/Lib_Generic.lua
@@ -1209,6 +1209,93 @@ end
 -----------------------------------------------------------------------------------------
 --
 -----------------------------------------------------------------------------------------
+function CF_AttemptReplaceLimb(actor, limb)
+	local j = 0
+	local isArm = string.find(limb.PresetName, " Arm")
+	local isLeg = string.find(limb.PresetName, " Leg")
+	local isHead = string.find(limb.PresetName, " Head")
+	local limbName = limb:StringValueExists("LimbName") and limb:GetStringValue("LimbName") or limb.PresetName
+	if isArm then
+		j = not actor.FGArm and 1 or (not actor.BGArm and 2 or j)
+	elseif isLeg then
+		j = not actor.FGLeg and 3 or (not actor.BGLeg and 4 or j)
+	elseif isHead and actor.Head == nil then
+		j = 5
+	end
+	if j ~= 0 then
+		local reference = CreateAHuman(actor:GetModuleAndPresetName())
+		local referenceLimb
+		if j == 1 then
+			referenceLimb = reference.FGArm
+		elseif j == 2 then
+			referenceLimb = reference.BGArm
+		elseif j == 3 then
+			referenceLimb = reference.FGLeg
+		elseif j == 4 then
+			referenceLimb = reference.BGLeg
+		elseif j == 5 then
+			referenceLimb = reference.Head
+		end
+		local newLimb
+		if isArm then
+			newLimb = CreateArm(limbName .. (j == 1 and " FG" or " BG"))
+		elseif isLeg then
+			newLimb = CreateLeg(limbName .. (j == 3 and " FG" or " BG"))
+		else
+			newLimb = limb:Clone() --(limbName);
+		end
+		if newLimb then
+			if referenceLimb then
+				newLimb.ParentOffset = referenceLimb.ParentOffset
+				local woundName = referenceLimb:GetEntryWoundPresetName()
+				if woundName ~= "" then
+					newLimb.ParentBreakWound = CreateAEmitter(woundName)
+				end
+			end
+			for wound in newLimb.Wounds do
+				if
+					math.floor(wound.ParentOffset.X - newLimb.JointOffset.X + 0.5) == 0
+					and math.floor(wound.ParentOffset.Y - newLimb.JointOffset.Y + 0.5) == 0
+				then
+					for em in wound.Emissions do
+						em.ParticlesPerMinute = 0
+					end
+					wound.Scale = wound.Scale * 0.7
+				end
+			end
+			if j == 1 then
+				actor.FGArm = newLimb
+			elseif j == 2 then
+				actor.BGArm = newLimb
+			elseif j == 3 then
+				actor.FGLeg = newLimb
+			elseif j == 4 then
+				actor.BGLeg = newLimb
+			elseif j == 5 then
+				actor.Head = newLimb
+			end
+			for wound in actor.Wounds do
+				if
+					math.floor(wound.ParentOffset.X - newLimb.ParentOffset.X + 0.5) == 0
+					and math.floor(wound.ParentOffset.Y - newLimb.ParentOffset.Y + 0.5) == 0
+				then
+					for em in wound.Emissions do
+						em.ParticlesPerMinute = 0
+					end
+					wound.Scale = wound.Scale * 0.7
+				end
+			end
+			limb:RemoveNumberValue("Carriable")
+		else
+			j = 0
+		end
+		DeleteEntity(reference)
+	end
+	return j ~= 0
+end
+-----------------------------------------------------------------------------------------
+--
+-----------------------------------------------------------------------------------------
 function CF_RandomizeLimbs(actor, limbs)
 	if IsAHuman(actor) then
 		actor = ToAHuman(actor)
